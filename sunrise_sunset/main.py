b24473624781905cecb4e735956fa8b0e8334f07
clnhrn <herniacln@gmail.com>
2022-10-15 01:30:15 +0800

    Modified for deployment to Google Cloud Run

--- sunrise_sunset/main.py
+++ sunrise_sunset/main.py
@@ -1,3 +1,4 @@
+import os
 import re
 from datetime import datetime
 
@@ -73,4 +74,5 @@ def get_the_time_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), threaded=True)
+
